022cc84a1150156bd83c26c6950f8682cae09410
Simao Belchior <simaobelchior@gmail.com>
2015-02-18 08:28:50 +0100

    added mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'our_milheones.simaobelchior.net',
                                                port: 80 }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
