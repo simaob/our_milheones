8de0a721b5bbadc66c7db1701d8fa5ad4bbc4aed
Simao Belchior <simaobelchior@gmail.com>
2015-02-20 20:23:15 +0100

    Added tests for recent bets

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,7 +24,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "recent bets should be empty if no bets" do
+    Bet.delete_all
+    assert_equal [], @user.recent_bets(5)
+  end
+
+  test "recent bets return all of users bets" do
+    bet = {numbers: "1,2,3,4,5", stars: "1,2"}
+    5.times do |i|
+      week = Week.create(number: i, friday: i.weeks.ago)
+      Bet.create(user_id: @user.id, week_id: week.id, bet: bet)
+    end
+    assert_equal 5, @user.recent_bets(5).size
+  end
+
+  test "recent bets return all but one users bets" do
+    bet = {numbers: "1,2,3,4,5", stars: "1,2"}
+    last_bet = nil
+    6.times do |i|
+      week = Week.create(number: i, friday: i.weeks.ago)
+      last_bet = Bet.create(user_id: @user.id, week_id: week.id, bet: bet)
+    end
+    assert_equal 1, (@user.bets.size - @user.recent_bets(5).size)
+    assert_equal last_bet, (@user.bets - @user.recent_bets(5)).first
+  end
 end
