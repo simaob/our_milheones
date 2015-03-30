de73e390c8c287d065b6b03f73336300898228f5
Simao Belchior <simaobelchior@gmail.com>
2015-03-30 16:06:27 +0100

    Try to keep mails in same thread

--- app/mailers/bet_mailer.rb
+++ app/mailers/bet_mailer.rb
@@ -13,27 +13,27 @@ class BetMailer < ApplicationMailer
     @week = week
     @quote = random_quote
     mail(to: @week.users_missing_bets.map(&:email).join(","),
-         subject: "[Euromilhões] #{@week.friday}")
+         subject: "Re: [Euromilhões] #{@week.friday}")
   end
 
   def prize_won week
     @week = week
     @quote = random_quote
     mail(to: User.order(:name).map(&:email).join(","),
-         subject: "[Euromilhões] #{@week.friday}")
+         subject: "Re: [Euromilhões] #{@week.friday}")
   end
 
   def bets_made week
     @week = week
     @quote = random_quote
     mail(to: User.order(:name).map(&:email).join(","),
-         subject: "[Euromilhões] #{@week.friday}")
+         subject: "Re: [Euromilhões] #{@week.friday}")
   end
 
   def do_the_bets week
     @week = week
     mail(to: User.where(is_admin: true).map(&:email).join(","),
-         subject: "[Euromilhões] #{@week.friday}")
+         subject: "Re: [Euromilhões] #{@week.friday}")
   end
 end
 
