0d57bd24c373b32db1efa975eddc49df0c28d990
Simao Belchior <simaobelchior@gmail.com>
2015-06-15 11:06:34 +0200

    Trying to fix bug with missing console

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,9 +27,11 @@ set :linked_files, fetch(:linked_files, []).
   push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids',
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids',
   'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system',
   'public/uploads')
+
+set :bundle_binstubs, nil
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
