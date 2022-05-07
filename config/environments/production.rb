0c13c37d7b5f61c5b88b920c663fa663acab07f8
Simao Belchior <simaobelchior@gmail.com>
2022-05-07 12:37:48 +0100

    Update domain throughout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'our_milheones.simaobelchior.net',
+  config.action_mailer.default_url_options = { host: 'our-milheones.simaobelchior.net',
                                                port: 80 }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
